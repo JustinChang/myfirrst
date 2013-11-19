7b27407cbc0e6a03f18be0962329046c9a38afeb
Justin <justin@ciMac.local>
2013-11-18 19:02:05 -0500

    Initial Commit

---
+++ myfirrst/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  myfirrst
+//
+//  Created by Justin on 11/18/13.
+//  Copyright (c) 2013 Justin Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
